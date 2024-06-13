e07a770ababe6d8adc477e876133ef45909cf7ce
pixelboogie <justamovie@yahoo.com>
2024-06-13 16:11:34 -0500

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFormsAndValidations.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult WeaklyTypedLogin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult LoginPost(string username, string password)
+        {
+            ViewBag.Username = username;
+            ViewBag.Password = password;
+            return View();
+        }
+    }
+}
